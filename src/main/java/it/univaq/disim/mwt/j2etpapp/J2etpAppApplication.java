9ff21c31c627a54b5d7122926feefdd6a69ee0ae
valentino <valentino.digiosaffatte@student.univaq.it>
2020-02-29 14:11:53 +0100

    init

---
+++ src/main/java/it/univaq/disim/mwt/j2etpapp/J2etpAppApplication.java
@@ -0,0 +1,13 @@
+package it.univaq.disim.mwt.j2etpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2etpAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J2etpAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/univaq/disim/mwt/j2etpapp/J2etpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univaq.disim.mwt.j2etpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2etpAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
