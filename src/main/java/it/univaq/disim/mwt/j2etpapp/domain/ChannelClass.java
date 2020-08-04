1cd91d0d960ecad32b30be207add9efa607b3fbd
valentino <valentino.digiosaffatte@student.univaq.it>
2020-08-04 19:01:02 +0200

    db build 2, fks 1to1 and created_at timestamp

--- src/main/java/it/univaq/disim/mwt/j2etpapp/domain/ChannelClass.java
+++ src/main/java/it/univaq/disim/mwt/j2etpapp/domain/ChannelClass.java
@@ -1,9 +1,12 @@
 package it.univaq.disim.mwt.j2etpapp.domain;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Set;
 
 @Data
@@ -27,12 +30,21 @@ public class ChannelClass implements Serializable {
     @Column(nullable = true, columnDefinition = "text")
     private String rules;
 
-    //@ManyToMany(mappedBy = "channels")
-    //private Set<User> users;
+    @OneToOne()
+    private ImageClass image;
 
-    // This is on MongoDB
-    //@OneToMany(mappedBy = "channel")
-    //private Set<Post> posts;
+    @OneToOne()
+    private UserClass creator;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "timestamp default current_timestamp")
+    private Date createdAt;
+
+    //@UpdateTimestamp
+    //@Temporal(TemporalType.TIMESTAMP)
+    //@Column(name = "updated_at", nullable = false, columnDefinition = "timestamp")
+    //private Date updatedAt = new Date();
 
     @OneToMany(mappedBy = "channel")
     private Set<UserChannelRole> userChannelRole;
