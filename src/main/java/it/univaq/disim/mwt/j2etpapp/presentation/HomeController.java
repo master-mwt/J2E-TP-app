5f2cc70121d1009156514d47e39ba3649402449c
valentino <valentino.digiosaffatte@student.univaq.it>
2020-08-19 17:53:35 +0200

    clean pre-ui

--- src/main/java/it/univaq/disim/mwt/j2etpapp/presentation/HomeController.java
+++ src/main/java/it/univaq/disim/mwt/j2etpapp/presentation/HomeController.java
@@ -1,23 +1,13 @@
 package it.univaq.disim.mwt.j2etpapp.presentation;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("")
     public String root() {
-        return "<h1>Welcome</h1>";
-    }
-
-    @GetMapping("/home")
-    public String home() {
-        return "<h1>Welcome logged</h1>";
-    }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return "<h1>Welcome admin</h1>";
+        return "index";
     }
 }
