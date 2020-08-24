aed8d2509d0427d9d52dc62598307679a749a237
ricdip <riccardodpr@gmail.com>
2020-08-24 19:44:46 +0200

    pagination (html, non jscroll)

--- src/main/java/it/univaq/disim/mwt/j2etpapp/presentation/HomeController.java
+++ src/main/java/it/univaq/disim/mwt/j2etpapp/presentation/HomeController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
 @Controller
 public class HomeController {
 
@@ -24,9 +22,9 @@ public class HomeController {
     private ChannelBO channelBO;
 
     @GetMapping("")
-    public String posts(Model model) {
-        List<PostClass> posts = postBO.findAllOrderByCreatedAtDesc();
-        model.addAttribute("posts", posts);
+    public String root(Model model) {
+        Page<PostClass> postFirstPage = postBO.findAllOrderByCreatedAtDescPaginated(0, 10);
+        model.addAttribute("page", postFirstPage);
         return "welcome";
     }
 
@@ -34,14 +32,14 @@ public class HomeController {
     @GetMapping(value = "/channels/page/{page}", produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
     public String channels(@PathVariable("page") int page) {
-        Page<ChannelClass> channelPage = channelBO.findByNameContainsPaginated("", page, 3);
+        Page<ChannelClass> channelPage = channelBO.findByNameContainsPaginated("", page, 10);
         return channelPage.toString();
     }
 
     // TODO: post test method return page with information inside (pagination)
     @GetMapping("/posts/page/{page}")
-    public String rootPaginated(@PathVariable("page") int page, Model model) {
-        Page<PostClass> postPage = postBO.findAllOrderByCreatedAtDescPaginated(page, 3);
+    public String postPaginated(@PathVariable("page") int page, Model model) {
+        Page<PostClass> postPage = postBO.findAllOrderByCreatedAtDescPaginated(page, 10);
         model.addAttribute("page", postPage);
         return "welcome";
     }
