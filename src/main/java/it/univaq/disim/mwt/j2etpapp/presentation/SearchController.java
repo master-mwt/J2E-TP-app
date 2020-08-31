11b590a1c32cc15ec772992dbde4113b4ef63c2d
ricdip <riccardodpr@gmail.com>
2020-08-31 17:34:20 +0200

    empty_res, search hold old values (target, query)

--- src/main/java/it/univaq/disim/mwt/j2etpapp/presentation/SearchController.java
+++ src/main/java/it/univaq/disim/mwt/j2etpapp/presentation/SearchController.java
@@ -32,25 +32,49 @@ public class SearchController {
     @GetMapping(value = "/search")
     public String search(@PathParam("target") String target, @PathParam("query") String query, Model model) {
         if(target != null){
+            if(query == null || query.equals("")){
+                model.addAttribute("emptySearch", true);
+                return emptySearch();
+            }
             switch (target){
                 case "posts":
                     Page<PostClass> postPage = postBO.findByTitleContainsPaginated(query, 0, 10);
+
+                    if(postPage.getContent().isEmpty()){
+                        return emptySearch();
+                    }
+
                     model.addAttribute("postPage", postPage);
                     model.addAttribute("channelBO", channelBO);
                     model.addAttribute("userBO", userBO);
                     return "pages/search_res/posts_res";
                 case "channels":
                     Page<ChannelClass> channelPage = channelBO.findByNameContainsPaginated(query, 0, 10);
+
+                    if(channelPage.getContent().isEmpty()){
+                        return emptySearch();
+                    }
+
                     model.addAttribute("channelPage", channelPage);
                     model.addAttribute("userChannelRoleBO", userChannelRoleBO);
                     model.addAttribute("roleBO", roleBO);
                     return "pages/search_res/channels_res";
                 case "users":
                     Page<UserClass> userPage = userBO.findByUsernameContainsPaginated(query, 0, 10);
+
+                    if(userPage.getContent().isEmpty()){
+                        return emptySearch();
+                    }
+
                     model.addAttribute("userPage", userPage);
                     return "pages/search_res/users_res";
                 case "tags":
                     Page<PostClass> postPageByTags = postBO.findByTagsContainsPaginated(new HashSet<>(tagBO.findByNameContains(query)), 0, 10);
+
+                    if(postPageByTags.getContent().isEmpty()){
+                        return emptySearch();
+                    }
+
                     model.addAttribute("postPage", postPageByTags);
                     model.addAttribute("channelBO", channelBO);
                     model.addAttribute("userBO", userBO);
@@ -65,7 +89,17 @@ public class SearchController {
 
     @GetMapping(value = "/search/posts/page/{page}")
     public String postPaginated(@PathVariable(name = "page") int page, @PathParam("query") String query, Model model) {
+        if(query == null || query.equals("")){
+            model.addAttribute("emptySearch", true);
+            return emptySearch();
+        }
+
         Page<PostClass> postPage = postBO.findByTitleContainsPaginated(query, page, 10);
+
+        if(postPage.getContent().isEmpty()){
+            return emptySearch();
+        }
+
         model.addAttribute("postPage", postPage);
         model.addAttribute("channelBO", channelBO);
         model.addAttribute("userBO", userBO);
@@ -74,7 +108,17 @@ public class SearchController {
 
     @GetMapping(value = "/search/channels/page/{page}")
     public String channelPaginated(@PathVariable(name = "page") int page, @PathParam("query") String query, Model model) {
+        if(query == null || query.equals("")){
+            model.addAttribute("emptySearch", true);
+            return emptySearch();
+        }
+
         Page<ChannelClass> channelPage = channelBO.findByNameContainsPaginated(query, page, 10);
+
+        if(channelPage.getContent().isEmpty()){
+            return emptySearch();
+        }
+
         model.addAttribute("channelPage", channelPage);
         model.addAttribute("userChannelRoleBO", userChannelRoleBO);
         model.addAttribute("roleBO", roleBO);
@@ -83,7 +127,17 @@ public class SearchController {
 
     @GetMapping(value = "/search/users/page/{page}")
     public String userPaginated(@PathVariable(name = "page") int page, @PathParam("query") String query, Model model) {
+        if(query == null || query.equals("")){
+            model.addAttribute("emptySearch", true);
+            return emptySearch();
+        }
+
         Page<UserClass> userPage = userBO.findByUsernameContainsPaginated(query, page, 10);
+
+        if(userPage.getContent().isEmpty()){
+            return emptySearch();
+        }
+
         model.addAttribute("userPage", userPage);
         model.addAttribute("userBO", userBO);
         return "pages/search_res/users_res";
@@ -91,10 +145,24 @@ public class SearchController {
 
     @GetMapping(value = "/search/tags/page/{page}")
     public String tagPaginated(@PathVariable(name = "page") int page, @PathParam("query") String query, Model model) {
+        if(query == null || query.equals("")){
+            model.addAttribute("emptySearch", true);
+            return emptySearch();
+        }
+
         Page<PostClass> postPage = postBO.findByTagsContainsPaginated(new HashSet<>(tagBO.findByNameContains(query)), page, 10);
+
+        if(postPage.getContent().isEmpty()){
+            return emptySearch();
+        }
+
         model.addAttribute("postPage", postPage);
         model.addAttribute("channelBO", channelBO);
         model.addAttribute("userBO", userBO);
         return "pages/search_res/tags_res";
+    }
+
+    private String emptySearch() {
+        return "pages/search_res/empty_res";
     }
 }
